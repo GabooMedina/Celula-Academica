7dab673534beb17a5e08fdcfd4d83edfd3f19e4d
xLexus <132799729+xLexus@users.noreply.github.com>
2024-04-25 10:04:40 -0500

    Revert "Implementación del Login"

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,24 +1,12 @@
 import React from "react";
 import { NavBar } from "../components/NavBar";
-import { LoginForm } from "../components/Loginform";
-import { Footer } from "../components/Footer";
-import '../App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Login(){
     return(
 
         <div>
-            <div>
             <NavBar />
-            </div>
-           
-            <div className="container1"> 
-            <LoginForm />
-            </div>
-            <div>
-            <Footer/>
-            </div>
-            
+            <h1>ESTE SERA EL LOGIN </h1>
         </div>
         
     );
